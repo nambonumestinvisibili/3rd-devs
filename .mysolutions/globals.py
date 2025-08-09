6e95107da40e62d968514b74d609591737facb4e
Dawid Holewa <dawid.holewa@axa.ie>
2025-08-09 14:22:33 +0200

    removed keys

--- .mysolutions/globals.py
+++ .mysolutions/globals.py
@@ -1,5 +1,7 @@
-openapi_api_key = 'REMOVEDJ7fwVLgN_4nAy6e9j7oJiYGRi8a0kUSxPBPc4zcdHBNvoIO11StxtrdtB2UWSwN2uc4VE78ZIET3BlbkFJdEnH4mP-ouyasj6GZj7WzbwJd5Ra6B1d-MhVW-uSEZU4XnCZKpXvOZuXb6WvUx_0YuSKuvhfcA'
-aidevs_api_key = 'aff1e95c-950e-4995-a153-e5bad44b1eae'
+import os
+
+openapi_api_key = os.getenv('OPENAPI_API_KEY')
+aidevs_api_key = os.getenv('AIDEVS_API_KEY')
 
 import types
 
